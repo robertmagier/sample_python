0c97397f25fff77f49e8b7e2e717dfd46be67fc2
robert.magier@gmail.com <robert.magier@gmail.com>
2020-02-11 22:40:23 -0500

    Removed some lines

--- test.py
+++ test.py
@@ -1,19 +1,7 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-# print(sys.version)
-# print(sys.executable)
-
-
-def greet(who_to_greet):
-    test = "test"
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
 
